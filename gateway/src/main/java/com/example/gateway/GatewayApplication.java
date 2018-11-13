8c6ae9a7a6a6865681340f4c7576cf8ca1ca29c3
Administrator <Administrator@PC-20171216YXBL>
2018-11-13 17:08:59 +0800

    study

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().route(
+				p -> p.path("/get").filters(fn -> fn.addRequestHeader("hello", "word")).uri("http://httpbin.org:80"))
+				.build();
+	}
+}
